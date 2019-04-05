926bf0b1ebee20084ae334cff23108c4a995756a
Bjoern Jaenichen <contact@beejayjayn.de>
2019-04-05 22:34:11 +0200

    First 4 sound files
    Signed-off-by: Bjoern Jaenichen <contact@beejayjayn.de>

---
+++ MyMedia.lua
@@ -0,0 +1,25 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "JainaHT", [[Interface\Addons\SharedMedia_BeeJay\sound\JainaHT.mp3]]) 
+LSM:Register("sound", "KellekRage", [[Interface\Addons\SharedMedia_BeeJay\sound\KellekRage.mp3]]) 
+LSM:Register("sound", "OW-WoW", [[Interface\Addons\SharedMedia_BeeJay\sound\OW-WoW.mp3]]) 
+LSM:Register("sound", "welcome", [[Interface\Addons\SharedMedia_BeeJay\sound\welcome.mp3]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
