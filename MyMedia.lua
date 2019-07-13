5050f643234256f7e06fb2cb12e0174d25ee9bfa
Bjoern Jaenichen <contact@beejayjayn.de>
2019-07-13 15:22:22 +0200

    add Blase @ Ashvane T25, implement standalone and colors

--- MyMedia.lua
+++ MyMedia.lua
@@ -15,10 +15,11 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   SOUND
 -- -----
-LSM:Register("sound", "JainaHT", [[Interface\Addons\SharedMedia_BeeJay\sound\JainaHT.mp3]]) 
-LSM:Register("sound", "KellekRage", [[Interface\Addons\SharedMedia_BeeJay\sound\KellekRage.mp3]]) 
-LSM:Register("sound", "OW-WoW", [[Interface\Addons\SharedMedia_BeeJay\sound\OW-WoW.mp3]]) 
-LSM:Register("sound", "welcome", [[Interface\Addons\SharedMedia_BeeJay\sound\welcome.mp3]]) 
+LSM:Register("sound", "|cFF44ee99DTB: Blase|r", [[Interface\Addons\SharedMedia_BeeJay\sound\Blase.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: JainaHT|r", [[Interface\Addons\SharedMedia_BeeJay\sound\JainaHT.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: KellekRage|r", [[Interface\Addons\SharedMedia_BeeJay\sound\KellekRage.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: OW-WoW|r", [[Interface\Addons\SharedMedia_BeeJay\sound\OW-WoW.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: Welcome|r", [[Interface\Addons\SharedMedia_BeeJay\sound\welcome.ogg]]) 
 
 -- -----
 --   STATUSBAR
