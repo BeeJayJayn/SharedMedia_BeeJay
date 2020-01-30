fa0b9d5724969949d01d63fd37a4d333f11345bf
Bjoern Jaenichen <contact@beejayjayn.de>
2020-01-30 15:11:30 +0100

    Added new Sounds, removed mp3 files

--- MyMedia.lua
+++ MyMedia.lua
@@ -20,7 +20,11 @@ LSM:Register("sound", "|cFF44ee99DTB: JainaHT|r", [[Interface\Addons\SharedMedia
 LSM:Register("sound", "|cFF44ee99DTB: KellekRage|r", [[Interface\Addons\SharedMedia_BeeJay\sound\KellekRage.ogg]]) 
 LSM:Register("sound", "|cFF44ee99DTB: OW-WoW|r", [[Interface\Addons\SharedMedia_BeeJay\sound\OW-WoW.ogg]]) 
 LSM:Register("sound", "|cFF44ee99DTB: Welcome|r", [[Interface\Addons\SharedMedia_BeeJay\sound\welcome.ogg]]) 
-
+LSM:Register("sound", "|cFF44ee99DTB: Halleluja|r", [[Interface\Addons\SharedMedia_BeeJay\sound\hallelujahshort.swf.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: B&G|r", [[Interface\Addons\SharedMedia_BeeJay\sound\bump-and-grind.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: Hey Listen|r", [[Interface\Addons\SharedMedia_BeeJay\sound\hey_listen.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: 2SAD4ME|r", [[Interface\Addons\SharedMedia_BeeJay\sound\2SAD4ME.ogg]]) 
+LSM:Register("sound", "|cFF44ee99DTB: Sex|r", [[Interface\Addons\SharedMedia_BeeJay\sound\i-just-had-sex.ogg]]) 
 -- -----
 --   STATUSBAR
 -- -----
